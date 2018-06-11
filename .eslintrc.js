e20ce97d47a8d1d268f857b40e3cb814d6f90f0c
Santiago M.A <santiagomartinagra@gmail.com>
2018-06-11 21:55:17 +0200

    style(eslintrc): nuevas reglas

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,21 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'vue/html-end-tags': 'error',
+    'vue/html-indent': [
+      'error',
+      2,
+      {'alignAttributesVertically': true}
+    ],
+    'vue/html-self-closing': 'error',
+    'vue/attributes-order': 'error',
+    'vue/html-quotes': [
+      'error',
+      'double'
+    ],
+    'vue/order-in-components': 'error',
+    'vue/require-default-prop': 'error',
+    'vue/require-prop-types': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
