00185f0f24ee5b7dc236a8e9070bb691e6d6e1ad
Santiago M.A <santiagomartinagra@gmail.com>
2018-06-11 18:48:49 +0200

    chore(main): config 🔥

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
+import firebase from 'firebase'
+
 import App from './App.vue'
+
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: 'AIzaSyBmWYhxl0-M-4MGR9zOH2JgTRMYo_7H-iY',
+  authDomain: 'e-sports-info.firebaseapp.com',
+  databaseURL: 'https://e-sports-info.firebaseio.com',
+  projectId: 'e-sports-info',
+  storageBucket: 'e-sports-info.appspot.com',
+  messagingSenderId: '420545083704'
+}
+firebase.initializeApp(config)
 
 new Vue({
   router,
