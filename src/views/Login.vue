3d8c6e2167736fa1532eaa0a17839e02d5112eee
Santiago M.A <santiagomartinagra@gmail.com>
2018-06-11 18:35:50 +0200

    refactor: Home -> Login

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form action="">
+      <input type="email" placeholder="Email">
+      <input type="passowrd" placeholder="password">
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
